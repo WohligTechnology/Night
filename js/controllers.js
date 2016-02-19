6822d8cdd364b3163426c2524a977d827e16273c
mahesh maurya <mahesh@wohlig.com>
2016-02-19 08:24:00 +0530

    ccc

--- js/controllers.js
+++ js/controllers.js
@@ -169,6 +169,77 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
   $scope.menutitle = NavigationService.makeactive("Events");
   TemplateService.title = $scope.menutitle;
   $scope.navigation = NavigationService.getnav();
+  $scope.today = function() {
+     $scope.dt = new Date();
+   };
+   $scope.today();
+
+   $scope.toggleMin = function() {
+     $scope.minDate = $scope.minDate ? null : new Date();
+   };
+
+   $scope.toggleMin();
+   $scope.maxDate = new Date(2020, 5, 22);
+
+   $scope.open1 = function() {
+     $scope.popup1.opened = true;
+   };
+
+   $scope.setDate = function(year, month, day) {
+     $scope.dt = new Date(year, month, day);
+   };
+
+   $scope.dateOptions = {
+     formatYear: 'yy',
+     startingDay: 1
+   };
+
+   $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+   $scope.format = $scope.formats[0];
+   $scope.altInputFormats = ['M!/d!/yyyy'];
+
+   $scope.popup1 = {
+     opened: false
+   };
+
+   $scope.getDayClass = function(date, mode) {
+     if (mode === 'day') {
+       var dayToCheck = new Date(date).setHours(0,0,0,0);
+
+       for (var i = 0; i < $scope.events.length; i++) {
+         var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+
+         if (dayToCheck === currentDay) {
+           return $scope.events[i].status;
+         }
+       }
+     }
+
+     return '';
+   };
+
+     $scope.open = function(size) {
+
+       var modalInstance = $uibModal.open({
+         animation: $scope.animationsEnabled,
+         templateUrl: 'views/modal/slider-link.html',
+         size: size,
+         resolve: {
+           items: function() {
+             return $scope.items;
+           }
+         }
+       });
+
+       modalInstance.result.then(function(selectedItem) {
+         $scope.selected = selectedItem;
+       }, function() {
+         $log.info('Modal dismissed at: ' + new Date());
+       });
+     };
+     $scope.toggleAnimation = function() {
+       $scope.animationsEnabled = !$scope.animationsEnabled;
+     };
 })
 
 .controller('BlogsCtrl', function($scope, TemplateService, NavigationService, $timeout, $log) {
