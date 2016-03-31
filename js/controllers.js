0718fb91190339af033c754884a6e2c601102b83
sohan1 <shn619@gmail.com>
2016-03-31 10:51:24 +0530

    piechart

--- js/controllers.js
+++ js/controllers.js
@@ -174,6 +174,42 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
   $scope.menutitle = NavigationService.makeactive("Dashboard");
   TemplateService.title = $scope.menutitle;
   $scope.navigation = NavigationService.getnav();
+
+  $scope.pieData = [{
+    name: "Microsoft Internet Explorer",
+    y: 56.33
+  }, {
+    name: "Chrome",
+    y: 24.03,
+    sliced: true,
+    selected: true
+  }, {
+    name: "Firefox",
+    y: 10.38
+  }, {
+    name: "Safari",
+    y: 4.77
+  }, {
+    name: "Opera",
+    y: 0.91
+  }, {
+    name: "Proprietary or Undetectable",
+    y: 0.2
+  }]
+  $scope.chartOptions = {
+    title: {
+      text: 'User data'
+    },
+    xAxis: {
+      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+      ]
+    },
+
+    series: [{
+      data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+    }]
+  };
 })
 
 
